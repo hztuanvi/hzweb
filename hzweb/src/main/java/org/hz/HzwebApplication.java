e07aab896f6e4d95585ff282fa2e9b674790cc74
OS <OS@192.168.1.2>
2018-06-05 20:56:55 +0700

    Commit full source code

---
+++ hzweb/src/main/java/org/hz/HzwebApplication.java
@@ -0,0 +1,12 @@
+package org.hz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HzwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HzwebApplication.class, args);
+	}
+}
